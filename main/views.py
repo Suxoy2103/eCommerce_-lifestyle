fdfad370d48d2a206ef30cce0903ffd96bde8768
Suxoy2103 <dimasushkevich@gmail.com>
2024-12-15 16:46:44 +0100

    about page style

--- main/views.py
+++ main/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        "title": "lifestyle | Online Fashion, Men`s, Women`s & Kid`s Clothes | lifestyle DE"
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        "title": "About us - Lifestyle Group"
+    }
+    return render(request, 'main/about.html', context)
